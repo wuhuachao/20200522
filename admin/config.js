f64444b408d3ba453650115fc3abd652944ddf71
wuhuachao <1601236641@qq.com>
2020-05-23 13:13:10 +0800

    new

--- admin/config.js
+++ admin/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	//=>WEB服务端口号
-	PORT: 8888,
+	PORT: 80,
 
 	//=>CROS跨域相关信息
 	CROS: {
-		ALLOW_ORIGIN: 'http://127.0.0.1:5500',//允许的这个客户端的地址
+		ALLOW_ORIGIN: 'https://wuhuachao.github.io/',//允许的这个客户端的地址
 		ALLOW_METHODS: 'PUT,POST,GET,DELETE,OPTIONS,HEAD',
 		HEADERS: 'Content-Type,Content-Length,Authorization, Accept,X-Requested-With',
 		CREDENTIALS: true
